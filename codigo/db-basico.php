1ceaa85f4afbd97faa49d5e5600343305fb27a00
ivan vejar <ivan.vejar@segic.usach.cl>
2021-04-13 15:22:46 -0400

    se agrega insert

--- codigo/db-basico.php
+++ codigo/db-basico.php
@@ -10,3 +10,24 @@ try{
      echo $e->getMessage();
 }
 
+//preparar consulta
+$sql = "INSERT into users
+       (full_name, email, user_name, password)
+values
+   (:full_name, :email, :user_name, :password)";
+
+   //statement
+
+   $stmt = $db->prepare($sql);
+
+  $full_name ='Ivan Vejar';
+  $email='ivan.vejar@segic.usach.cl'; 
+  $user_name='ivan.vejar';
+  $password='ivan123';
+
+  $stmt->bindParam(':full_name', $full_name);
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':user_name', $user_name);
+  $stmt->bindParam(':password', $password);
+
+  $stmt->execute();
