806d57b86b378b214130ba57292e18fff16fded9
ivan vejar <ivan.vejar@segic.usach.cl>
2021-04-16 08:33:00 -0400

    se agrega nuevas sentencias

--- codigo/db-basico.php
+++ codigo/db-basico.php
@@ -9,35 +9,60 @@ try{
 } catch(PDOException $e){
      echo $e->getMessage();
 }
-/*
-//insert
-//preparar consulta
-$sql = "INSERT into users
-       (full_name, email, user_name, password)
-values
-   (:full_name, :email, :user_name, :password)";
 
-   //statement
+// Insert Masivo
+/*$users = [
+    [
+        'name' => 'Miguel Perez',
+        'email' => 'miguel.perez@segic.cl',
+        'username' => 'miguel.perez',
+        'password' => 'miguel123'
+    ],
+    [
+        'name' => 'Andrea Perez',
+        'email' => 'andrea.perez@segic.cl',
+        'username' => 'andrea.perez',
+        'password' => 'andrea123'
+    ],
+];
+$sql = "INSERT INTO users 
+            (full_name, email, user_name, password)
+        VALUES
+            (:full_name, :email, :user_name, :password)";
+//statement
+$stmt = $db->prepare($sql);
+foreach ($users as $user) {
+    $stmt->bindParam(':full_name', $user['name']);
+    $stmt->bindParam(':email', $user['email']);
+    $stmt->bindParam(':user_name', $user['username']);
+    $stmt->bindParam(':password', password_hash($user['password'], PASSWORD_DEFAULT));
+    $stmt->execute();
+}*/
 
-   $stmt = $db->prepare($sql);
+// delete
+/*$id = 2;
+$stmt = $db->prepare("DELETE FROM users WHERE id=:id");
+$stmt->bindParam(':id', $id);
+$stmt->execute();
+*/
 
-  $full_name ='Ivan Vejar';
-  $email='ivan.vejar@segic.usach.cl'; 
-  $user_name='ivan.vejar';
-  $password='ivan123';
+// querying data
+$stmt = $db->prepare("SELECT * FROM users");
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  $stmt->bindParam(':full_name', $full_name);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':user_name', $user_name);
-  $stmt->bindParam(':password', $password);
+echo '<table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Username</th>
+        </tr>';
 
-  $stmt->execute();
-  */
-
-  //Delete
-  $id = 2;
-  $stmt = $db->prepare("DELETE FROM users WHERE id=:id");
-  $stmt->bindParam(':id', $id);
-  $stmt->execute(); 
-
-
+foreach ($users as $user) {
+    echo '<tr>
+            <td>' . $user['id'] . '</td>
+            <td>' . $user['full_name'] . '</td>
+            <td>' . $user['user_name'] . '</td>
+          </tr>';
+}
+echo '</table>';
