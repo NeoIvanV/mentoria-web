c2187b1459fc9cb7ddb6515adf845cb5ae1fded8
ivan vejar <ivan.vejar@segic.usach.cl>
2021-04-16 10:34:59 -0400

    se cambia titulo y sentencia php

--- codigo/index.php
+++ codigo/index.php
@@ -1,7 +1,12 @@
+<?
+// Sentencia PHP
+echo "Hola mundo";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V13</title>
+	<title>Registro Mentoria Web</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
