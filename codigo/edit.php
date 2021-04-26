1485790a1566c4690eec4e054537ea96e45e701f
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-04-26 19:09:43 -0400

    se actualiza editar

--- codigo/edit.php
+++ codigo/edit.php
@@ -35,7 +35,16 @@ if (isset($_POST['btn btn-primary'])) {
 
 	$message = "Registro actualizado con éxito";
 	$valido = 1;
-} 
+} else{
+    $sql = "SELECT * FROM users";
+
+    //statement
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+}
 
 ?>
 
