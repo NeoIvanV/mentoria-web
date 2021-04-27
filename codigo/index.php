303179fa03d5602af3b7c5fc704461599f4568d8
ivan.vejar <ivan.vejar@segic.usach.cl>
2021-04-26 21:46:39 -0400

    se actualiza editar

--- codigo/index.php
+++ codigo/index.php
@@ -85,7 +85,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <a href="edit.php?id=<?=$user['id']?>"><button class="btn btn-primary btn-sm">Modificar</button></a>
                         <a href="delete.php?id=<?=$user['id']?>"><button class="btn btn-primary btn-sm">Eliminar</button></a>
 				        <input name="nomreq" type="hidden" value="{{$archivo.nombre}}" />
-                        <a href="#" onclick="return confirm('Estás seguro que deseas eliminar el registro?');"><button class="btn btn-primary btn-sm">Eliminar</button>Eliminar registro</a>
+                        <a href="#" onclick="return confirm('Estás seguro que deseas eliminar el registro?');"><button class="btn btn-primary btn-sm">Eliminar</button></a>
                     </td>
                  </tr>
                <?php endforeach;?>         
